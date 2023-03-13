1e4b08aa17de7ffb5d78fab84a1e40e606f554c1
RP_Carbo <ralphpablo.carbo@wvsu.edu.ph>
2023-03-13 22:33:42 +0800

    added act1-4 for streamlit except only trans and rotate in act4 and no quiz1 conversion

--- pages/REALActivity3.py
+++ pages/REALActivity3.py
@@ -1,50 +1,30 @@
+import streamlit as st
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+st.title("Image Editor")
+
 # CHOICE 1
-def translate_image():
-    filename = input("Input image: ")
-    image = cv2.imread(filename)
-    dx = int(input("Enter the amount to translate along the X axis: "))
-    dy = int(input("Enter the amount to translate along the Y axis: "))
+def translate_image(image, dx, dy):
     translation_matrix = np.float32([[1, 0, dx], [0, 1, dy]])
     translated_image = cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0]))
-    plt.imshow(cv2.cvtColor(translated_image, cv2.COLOR_BGR2RGB))
-    plt.title("Transformed Image")
-    plt.show()
+    return translated_image
 
 # CHOICE 2
-def rotate_image():
-    filename = input("Input image: ")
-    image = cv2.imread(filename)
-    angle = float(input("Enter the rotation angle in degrees: "))
+def rotate_image(image, angle):
     height, width = image.shape[:2]
     rotation_matrix = cv2.getRotationMatrix2D((width/2, height/2), angle, 1)
     rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
-    plt.imshow(cv2.cvtColor(rotated_image, cv2.COLOR_BGR2RGB))
-    plt.title("Image Rotation")
-    plt.show()
+    return rotated_image
 
 # CHOICE 3
-def scale_image():
-    filename = input("Input image: ")
-    image = cv2.imread(filename)
-    print("Please enter a number between decimals (0.05 ~ 1.5)") # REMEMBER PUT DECIMALS OR YOU FREEZE
-    fx = float(input("Enter the scaling factor for X axis: "))
-    fy = float(input("Enter the scaling factor for Y axis: "))
+def scale_image(image, fx, fy):
     scaled_image = cv2.resize(image, None, fx=fx, fy=fy, interpolation=cv2.INTER_LINEAR)
-    plt.imshow(cv2.cvtColor(scaled_image, cv2.COLOR_BGR2RGB))
-    plt.title("Image Scaling")
-    plt.show()
+    return scaled_image
 
 # CHOICE 4
-def reflect_image():
-    filename = input("Input image: ")
-    image = cv2.imread(filename)
-    print("Enter flip direction: ")
-    print("1 for horizontal | 2 for vertical | 3 for both: ")
-    flip_direction = input("")
+def reflect_image(image, flip_direction):
     if flip_direction == '1':
         flipped_image = cv2.flip(image, 1)
     elif flip_direction == '2':
@@ -54,60 +34,50 @@ def reflect_image():
     else:
         print("Invalid direction entered")
         return
-    plt.imshow(cv2.cvtColor(flipped_image, cv2.COLOR_BGR2RGB))
-    plt.title("Image Reflection")
-    plt.show()
+    return flipped_image
 
 # CHOICE 5
-def shear_image():
-    filename = input("Input image: ")
-    image = cv2.imread(filename)
-    print("Please enter a number between decimals (0.05 ~ 1.5)") # PUT DECIMALS OR IT FREEZES BECAUSE ITS TOO BIG
-    sx = float(input("Enter the shear factor along the X axis: "))
-    sy = float(input("Enter the shear factor along the Y axis: "))
+def shear_image(image, sx, sy):
     shear_matrix = np.float32([[1, sx, 0], [sy, 1, 0]])
     sheared_image = cv2.warpAffine(image, shear_matrix, (image.shape[1], image.shape[0]))
-    plt.imshow(cv2.cvtColor(sheared_image, cv2.COLOR_BGR2RGB))
-    plt.title("Image Shear")
-    plt.show()
+    return sheared_image
 
-# PICK THEE CHOICES
+# MAIN FUNCTION
 def main():
-    while True:
-        print("What do you like to do?")
-        print("1 - Image translation")
-        print("2 - Image rotation")
-        print("3 - Image scaling")
-        print("4 - Image reflection")
-        print("5 - Image shear")
-        print("6 - Exit")
-        choice = input("")
-        if choice == '1':
-            translate_image()
-        elif choice == '2':
-            rotate_image()
-        elif choice == '3':
-            scale_image()
-        elif choice == '4':
-            reflect_image()
-        elif choice == '5':
-            shear_image()
-        elif choice == '6':
-            break
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        st.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), caption="Original Image", use_column_width=True)
+        choice = st.selectbox("What do you want to do?", ["", "Image translation", "Image rotation", "Image scaling", "Image reflection", "Image shear"])
+        if choice == "Image translation":
+            dx = st.number_input("Enter the amount to translate along the X axis:", value=0, step=1)
+            dy = st.number_input("Enter the amount to translate along the Y axis:", value=0, step=1)
+            translated_image = translate_image(image, dx, dy)
+            st.image(cv2.cvtColor(translated_image, cv2.COLOR_BGR2RGB), caption="Translated Image", use_column_width=True)
+        elif choice == "Image rotation":
+            angle = st.number_input("Enter the rotation angle in degrees:", value=0, step=1)
+            rotated_image = rotate_image(image, angle)
+            st.image(cv2.cvtColor(rotated_image, cv2.COLOR_BGR2RGB), caption="Rotated Image", use_column_width=True)
+        elif choice == "Image scaling":
+            scale_x = st.number_input("Enter the scaling factor along the X axis:", value=1.0, step=0.1)
+            scale_y = st.number_input("Enter the scaling factor along the Y axis:", value=1.0, step=0.1)
+            scaled_image = scale_image(image, scale_x, scale_y)
+            st.image(cv2.cvtColor(scaled_image, cv2.COLOR_BGR2RGB), caption="Scaled Image", use_column_width=True)
+        elif choice == "Image reflection":
+            axis = st.selectbox("Select reflection axis:", ["", "Horizontal", "Vertical"])
+        if axis == "Horizontal":
+            reflected_image = reflect_image(image, 1)
+            st.image(cv2.cvtColor(reflected_image, cv2.COLOR_BGR2RGB), caption="Horizontally Reflected Image", use_column_width=True)
+        elif axis == "Vertical":
+            reflected_image = reflect_image(image, 0)
+            st.image(cv2.cvtColor(reflected_image, cv2.COLOR_BGR2RGB), caption="Vertically Reflected Image", use_column_width=True)
+        elif choice == "Image shear":
+            shear_x = st.number_input("Enter the shear factor along the X axis:", value=0.0, step=0.1)
+            shear_y = st.number_input("Enter the shear factor along the Y axis:", value=0.0, step=0.1)
+            sheared_image = shear_image(image, shear_x, shear_y)
+            st.image(cv2.cvtColor(sheared_image, cv2.COLOR_BGR2RGB), caption="Sheared Image", use_column_width=True)
         else:
-            print("Invalid choice")
+            st.warning("Please upload an image.")
 
-        # LOOP GOING BACK TO CHOICES
-        while True:
-            answer = input("Do you want to edit another image? (Y/N): ")
-            if answer.lower() == 'y':
-                break
-            elif answer.lower() == 'n':
-                print("--- E N D ---")
-                return
-            else:
-                print("Invalid choice, please enter Y or N.")
-
-if __name__ == "__main__":
-    main()
-        
+if name == "main":
+main()
